a74f26a26fc8817b59f6cd5dee80fce253eafdfb
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-12-27 20:03:36 +0800

    Added the @material-ui css to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from "react";
+import {
+  Container,
+  AppBar,
+  Typography,
+  Grow,
+  Grid,
+  Button,
+} from "@material-ui/core";
 
 const App = () => {
+  let memories =
+    "https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI";
+
   return (
-    <div>
+    <Container maxidth="lg">
+      <AppBar position="static" color="ingerit">
+        <Typography align="center" variant="h2">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
       <h1>Hi</h1>
-    </div>
+      <Button
+        variant="contained"
+        color="primary"
+        target="_blank"
+        href="https://www.youtube.com/watch?v=ngc9gnGgUdA&ab_channel=JavaScriptMastery"
+      >
+        tutorial web
+      </Button>
+    </Container>
   );
 };
 
