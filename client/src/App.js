194677804189db6298b89e9ef9b9df61289aad20
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2022-01-06 18:33:57 +0800

    20220106 changed some client side format.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { Container, AppBar, Typography, Grow, Grid, Button } from "@material-ui/core";
-import Form from "./components/Form/Form";
-import Posts from "./components/Posts/Posts";
-import useStyles from "./styles";
+import React from "react";
+import { Container } from "@material-ui/core";
+// import Form from "./components/Form/Form";
+// import Posts from "./components/Posts/Posts";
+
 // Dispatch an action
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
+
 import Nav from "./components/Nav/Nav";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  const [currentId, setCurrentId] = useState(null);
-
-  // console.log(classes);
-
-  // console.log(getPosts);
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxWidth="lg">
-      <Nav />
-      <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={4}>
-            <Grid item xd={12} sm={7}>
-              <Posts currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xd={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-      <Button variant="contained" color="primary" target="_blank" href="https://www.youtube.com/watch?v=ngc9gnGgUdA&ab_channel=JavaScriptMastery">
-        tutorial web
-      </Button>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,17 +1,37 @@
 import React from "react";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const classes = useStyles();
+  const user = null;
 
   return (
     <AppBar className={classes.appBar} position="static" color="ingerit">
-      <Typography align="center" variant="h2">
-        My Blog
-      </Typography>
-      <img className={classes.image} src={memories} alt="memories" height="60" />
+      <div className="{classes.brandContainer}">
+        <Typography component={Link} to="/" align="center" variant="h2" align="center">
+          My Blog
+        </Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
